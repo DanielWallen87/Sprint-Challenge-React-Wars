1289ccab1f27bcb07dddc7d150f807cf156731f3
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-08-18 14:45:01 -0400

    Why was I making this so hard? No idea.

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -1,22 +1,31 @@
 import React from "react";
-import styled from "styled-components";
-import Name from "./Name";
-import Hair from "./Hair";
-import Eyes from "./Eyes";
-import Sex from "./Sex";
+import styled from 'styled-components';
 
-const CardBorder = styled.div`
-    border: 2px solid white;
+const GridContainer = styled.div`
+    display: inline-grid;
+    grid-template-columns: auto auto auto;
+    background-color: transparent;
+    padding: 10px;
+`
+
+const GridItem = styled.div`
+    background-color: rgba(255, 255, 255, 0.8);
+    border: 1px solid rgba(0, 0, 0, 0.8);
+    padding: 20px;
 `
 
 const Card = props => {
     return (
-        <CardBorder>
-            <Name name={props.name} />
-            <Hair hair={props.hair} />
-            <Eyes eyes={props.eyes} />
-            <Sex sex={props.sex} />
-        </CardBorder>
+        <GridContainer>
+            <GridItem>
+            <h1>{props.info.name}</h1> 
+            <p>Gender: {props.info.gender}</p>
+            <p>Height: {props.info.height} inches</p>
+            <p>Weight: {Math.floor(props.info.mass * 2.2)} lbs</p>
+            <p>Hair Color: {props.info.hair_color}</p>
+            <p>Eye Color: {props.info.eye_color}</p>
+            </GridItem>
+        </GridContainer>
     )
 }
 
